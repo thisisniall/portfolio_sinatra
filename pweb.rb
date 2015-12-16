ec31d1b2cd116314c3feb6cdca208d68a35d3e0d
Niall <niallpatrickmurphy@gmail.com>
2015-12-16 17:47:25 -0500

    fixed all issues, portfolio fully imported into sinatra.

--- pweb.rb
+++ pweb.rb
@@ -1,23 +1,21 @@
 require 'sinatra'
 
-titles = {:home => "Index", :about => "About", :clickbait => "Clickbait"}
-backgrounds = {:home => "/imgs/islandrange-7.jpg", :about => "/imgs/montanahills.jpg", :clickbait => "/imgs/keywest-5031.jpg"}
-
 get '/' do
 	@title = "Index"
-	@background = "/imgs/islandrange-7.jpg"
+	# due to layout.erb construction these are the css background ids
+	@background = "index"
 	erb :home
 end
 
 get '/about' do
 	@title = "About"
-	@background = backgrounds[:about]
+	@background = "about"
 	erb :about
 end
 
 get '/clickbait' do
 	@title = "Clickbait"
-	@background = backgrounds[:clickbait]
+	@background = "clickbait"
 	erb :clickbait
 end
 
